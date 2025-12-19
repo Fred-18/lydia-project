a5f2bfeb250d6bb9408f0ee2cab04fda9913242a
fred <frednobre18@gmail.com>
2025-12-19 22:51:46 +0100

    refacto useFilteredTransactions

--- src/app/hook/useFilteredTransactions.ts
+++ src/app/hook/useFilteredTransactions.ts
@@ -4,12 +4,12 @@ import { Transaction } from "../types/transaction";
 export function useFilteredTransactions(transactions: Transaction[]) {
   const [searchTerm, setSearchTerm] = useState("");
 
-  const filteredData = transactions.filter((item) =>
+  const filteredTransactions = transactions.filter((item) =>
     item.label.toLowerCase().includes(searchTerm.toLowerCase())
   );
   return {
     searchTerm,
     setSearchTerm,
-    filteredData
+    filteredData: filteredTransactions
   };
 }
