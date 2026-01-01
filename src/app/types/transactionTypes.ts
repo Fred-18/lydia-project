af0764833a97f9ae5651f7a4165c97e50dd06a89
fred <frednobre18@gmail.com>
2026-01-01 15:52:29 +0100

    rename transactionType

---
+++ src/app/types/transactionTypes.ts
@@ -0,0 +1,10 @@
+export type Transaction = {
+  label: string;
+  memberId?: number;
+  status?: string;
+  amount?: string;
+  date?: number;
+  paymentId?: string;
+  firstName?: string;
+  lastName?: string;
+};
